a44e5dbec5be3370216595826546e82e4e964103
Alejandro Ciniglio <mail@alejandrociniglio.com>
2012-10-16 10:26:25 -0400

    Added a test function to check performance of the spam filter.

--- src/spam/reader.clj
+++ src/spam/reader.clj
@@ -9,18 +9,23 @@
   (remove-dotfiles
    (map #(.getAbsolutePath %) (rest (file-seq (file "spam-corpus"))))))
 
-(defn train-spam [word-set]
-  (let [files (spam-set)]
-    (reduce #(train %1 (slurp %2) :spam) word-set files)))
-
-(defn test-train-spam []
-  (let [files (spam-set)]
-    (extract-words (slurp (first files)))))
-
 (defn ham-set []
   (remove-dotfiles
    (map #(.getAbsolutePath %) (rest (file-seq (file "ham-corpus"))))))
 
+(defn test-ham-set []
+  (remove-dotfiles
+   (map #(.getAbsolutePath %) (rest (file-seq (file "ham-test"))))))
+
+(defn train-spam [word-set]
+  (let [files (spam-set)]
+    (reduce #(train %1 (slurp %2) :spam) word-set files)))
+
 (defn train-ham [word-set]
   (let [files (ham-set)]
     (reduce #(train %1 (slurp %2) :ham) word-set files)))
+
+(defn test-ham [word-set]
+  (let [files (test-ham-set)]
+    (doseq [f files]
+      (println (score word-set (slurp f))))))
